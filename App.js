13bd49c5baf546cd83e915a83f1027bb1c7aafd2
Daiki Matsui <mti.daiki@gmail.com>
2019-12-12 12:21:12 +0900

    add bananas

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+class HelloWorldApp extends Component {
   render() {
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Helo, world!</Text>
         </View>
+    );
+  }
+}
+
+export default class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+        <Image source={pic} style={{width: 400, height: 200}}/>
     );
   }
 }
