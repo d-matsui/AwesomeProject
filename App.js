df94bb3cc275220352ef838e229986119f25b947
Daiki Matsui <mti.daiki@gmail.com>
2019-12-12 14:41:23 +0900

    add blink, styles

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 class HelloWorldApp extends Component {
   render() {
@@ -32,13 +32,78 @@ class Greeting extends Component {
   }
 }
 
-export default class LotsOfGreetings extends Component {
+class LotsOfGreetings extends Component {
   render() {
     return (
         <View>
         <Greeting name='Rexxar' />
         <Greeting name='Jaina' />
         <Greeting name='Valeera' />
+        </View>
+    )
+  }
+}
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    // state object
+    this.state = { isShowingText: true };
+  }
+
+  componentDidMount() {
+    // Toggle the state every second
+    setInterval(() => {
+      // this.setState(() => (
+      //   { isShowingText: !this.state.isShowingText }
+      // ))
+      this.setState((previousState) => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    }, 1000);
+  }
+
+  render() {
+    if (!this.state.isShowingText) {
+      return null;
+    }
+    return (
+        <Text>{this.props.text}</Text>
+    );
+  }
+}
+
+class BlinkApp extends Component {
+  render() {
+    return (
+        <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+        </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
+
+export default class LotsOfStyles extends Component {
+  render() {
+    return (
+        <View>
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigBlue}>just bigBlue</Text>
+        <Text style={[styles.bigBlue, styles.red]}>big blue, then red</Text>
         </View>
     )
   }
